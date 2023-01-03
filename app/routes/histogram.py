2c2e1404bacbf4cac2e3814cc6b51c6fce5ef368
Giacomo <monni.giacomo1998@gmail.com>
2023-01-03 17:01:36 +0100

    Some minor changes

--- app/routes/histogram.py
+++ app/routes/histogram.py
@@ -1,14 +1,12 @@
 from flask import render_template
 from app import app
-from app.forms.histogram_form import HistrogramForm
-from config import Configuration
-
-config = Configuration()
+from app.forms.histogram_form import HistogramForm
 
 @app.route('/histogram', methods=['GET', 'POST'])
+
 def histogram():
     """API for selecting an image and return its histogram"""
-    form = HistrogramForm()
+    form = HistogramForm()
     if form.validate_on_submit():  # POST
         image_id = form.image.data
         
@@ -17,4 +15,4 @@ def histogram():
 
     # otherwise, it is a get request and should return the
     # image selector
-    return render_template('histogram_select.html', form=form)
+    return render_template("histogram_select.html", form=form)
