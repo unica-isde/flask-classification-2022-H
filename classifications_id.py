1664c42be115d28428cff22990e9c643bc7b4f99
Michela Lecca <61116769+lccmic@users.noreply.github.com>
2023-01-25 17:01:43 +0100

    Add files via upload

---
+++ classifications_id.py
@@ -0,0 +1,30 @@
+import redis
+from rq import Connection, Queue
+
+from flask import jsonify
+from app import app
+from config import Configuration
+
+config = Configuration()
+
+
+@app.route('/classifications/<string:job_id>', methods=['GET'])
+def classifications_id(job_id):
+    """Returns the status and the result of the job identified
+    by the id specified in the path."""
+    redis_url = Configuration.REDIS_URL
+    redis_conn = redis.from_url(redis_url)
+    with Connection(redis_conn):
+        q = Queue(name=Configuration.QUEUE)
+        task = q.fetch_job(job_id)
+
+    response = {
+        'task_status': task.get_status(),
+        'data': task.result,
+    }
+    return response
+
+@app.route("/downloadJSON/<string:job_id>", methods=['GET'])
+def downloadJSON(job_id):
+    data = classifications_id(job_id)['data']
+    return (data)
