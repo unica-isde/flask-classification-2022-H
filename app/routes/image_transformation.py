85a80d0a4e6b792090ec2ad4262a8ef37ce63375
Matteo Pusceddu <mattswayqt@gmail.com>
2023-03-25 14:49:55 +0100

    refactor: removing logs and adding comments

--- app/routes/image_transformation.py
+++ app/routes/image_transformation.py
@@ -10,20 +10,23 @@ config = Configuration()
 @app.route('/image_transformation', methods=['GET', 'POST'])
 def image_transformation():
     form = ImageTransformationForm()
+    # Validate the input 
     if form.validate_on_submit():
+        # Get date from the form
         image_id = form.image.data
         transformation_id = form.transformation.data
         transformation_value = form.transformation_value.data
 
-        print('Image: ' + image_id)
-        print('Transformation: ' + transformation_id)
-        print('Value: ' + str(transformation_value))
-
-        transformed_image = transform_image(image_id=image_id, transformation=transformation_id, value=transformation_value)
+        # Transform the image
+        transformed_image = transform_image(image_id=image_id, transformation=transformation_id, 
+            value=transformation_value)
+        # Get a showable image in webpage after the transformation
         image = get_showable_image(transformed_image)
 
+        # Return the output template
         return render_template('image_transformation_output.html', transformed_image=image)
 
+    # Input is not validated, stay in the form template
     return str(render_template('image_transformation_select.html', 
                            form=form)).replace('step="any"', 'step="0.01"')
 
--- ml/image_transformation_utils.py
+++ ml/image_transformation_utils.py
@@ -3,13 +3,16 @@ from PIL import ImageEnhance, Image
 from base64 import b64encode
 from io import BytesIO
 
+# Transformation names array
 transformations = ['COLOR', 'BRIGHTNESS', 'CONTRAST', 'SHARPNESS']
 
+# Transform the image (specified by image_id) applying the transformation at specified value
 def transform_image(image_id, transformation, value):
     image = fetch_image(image_id=image_id)
     transformation_effect = transform_step(image, transformation)
     return transformation_effect.enhance(value)
-    
+
+# Transform the given image applying the transformation    
 def transform_step(image, transformation):
     if transformation == transformations[0]:
         return ImageEnhance.Color(image)
@@ -20,6 +23,7 @@ def transform_step(image, transformation):
     elif transformation == transformations[3]:
         return ImageEnhance.Sharpness(image)
 
+# Convert image from bytes to png base64 encoded image
 def get_showable_image(image:Image):
     image_data = BytesIO()
     image.save(image_data, 'PNG')
